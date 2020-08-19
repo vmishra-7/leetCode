ed4a20e2dc854fdedf91d488eba052ce9e48e3c3
vmishra-7 <42261169+vmishra-7@users.noreply.github.com>
2020-08-19 18:31:43 +0530

    Merge k Sorted Lists | LeetCode

---
+++ Day 1 Codes/mergeKLists.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
+        if(head1 == NULL) {
+            return head2;
+        }
+        if(head2 == NULL) {
+            return head1;
+        }
+        if(head1 == NULL && head2 == NULL) {
+            return NULL;
+        }
+        ListNode* FH = NULL;
+        ListNode* FT = NULL;
+        if(head1 -> val < head2 -> val) {
+            FH = head1;
+            FT = head1;
+            head1 = head1 -> next;
+        } else {
+            FH = head2;
+            FT = head2;
+            head2 = head2 -> next;
+        }
+        while(head1 != NULL && head2 != NULL) {
+            if(head1 -> val < head2 -> val) {
+                FT -> next = head1;
+                FT = head1;
+                head1 = head1 -> next;
+            } else {
+                FT -> next = head2;
+                FT = head2;
+                head2 = head2 -> next;
+            }
+        }
+        if(head1 != NULL) {
+            FT -> next = head1;
+
+        }
+        if(head2 != NULL) {
+            FT -> next = head2;
+        }
+        return FH;
+        }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() == 0) {
+            return NULL;
+        }
+        for(int i = 1; i < lists.size(); i++) {
+            lists[0] = mergeTwoLists(lists[0], lists[i]);
+        }
+        return lists[0];
+    }
+};
