86cc4579aa88df014a0394d35ab2f0ca90210a3f
vmishra-7 <42261169+vmishra-7@users.noreply.github.com>
2020-09-27 23:17:09 +0530

    Word Search II | LeetCode

--- Day 1 Codes/findWords.cpp
+++ Day 1 Codes/findWords.cpp
@@ -1,64 +1,76 @@
-class TrieNode{
-public:
-    bool is_end;
-    vector<TrieNode*> children;
-    TrieNode(){
-        is_end=false;
-        children=vector<TrieNode*>(26, NULL);
-    }   
-};
 
-class Trie{
-public:
-    TrieNode* getRoot(){return root;}
-    Trie(vector<string>& words){
-        root=new TrieNode();
-        for(int i=0; i<words.size(); ++i)
-            addWord(words[i]);
-    }
-    void addWord(const string& word){
-        TrieNode* cur=root;
-        for(int i=0; i<word.size(); ++i){
-            int index=word[i]-'a';
-            if(cur->children[index]==NULL)   
-               cur->children[index]=new TrieNode();
-            cur=cur->children[index];    
+class Trie {
+    public :
+    vector<Trie*> children;
+    bool isWord;
+    Trie() {
+        for(int i = 0; i < 26; i++) {
+            children.push_back(NULL);
         }
-        cur->is_end=true;
+        isWord = false;
     }
-private:
-    TrieNode* root;
+};
+class trieFunction {
+    public :
+    void addWords(Trie *root, string word) {
+        Trie *cur = root;
+        for(int i = 0; i < word.size(); i++) {
+            int index = word[i] - 'a';
+            if(cur -> children[index] == NULL) {
+                cur -> children[index] = new Trie();
+            }
+            cur = cur -> children[index];
+        }
+        cur -> isWord = true;
+    }
+    void WS(vector<vector<char>>& board, Trie *root, int i, int j, int N, int M, set<string> &output, string temp) {
+        int index = board[i][j] - 'a';
+        if(root -> children[index] == NULL) {
+            return;
+        }
+        root = root -> children[index];
+        temp += board[i][j];
+        if(root -> isWord) {
+            output.insert(temp);
+        }
+        char ch = board[i][j];
+        board[i][j] = '*';
+        if(i - 1 >= 0 && board[i - 1][j] != '*') {
+            WS(board, root, i - 1, j, N, M, output, temp);
+        }
+        if(i + 1 < N && board[i + 1][j] != '*') {
+            WS(board, root, i + 1, j, N, M, output, temp);
+        }
+        if(j - 1 >= 0 && board[i][j - 1] != '*') {
+            WS(board, root, i, j - 1, N, M, output, temp);
+        }
+        if(j + 1 < M && board[i][j + 1] != '*') {
+            WS(board, root, i, j + 1, N, M, output, temp);
+        }
+        board[i][j] = ch;
+    }
 };
 
 class Solution {
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        Trie* trie = new Trie(words);
-        TrieNode* root=trie->getRoot();
-        set<string> result_set;
-        for(int x=0; x<board.size(); ++x)
-            for(int y=0; y<board[0].size(); ++y)
-                findWords(board, x, y, root, "", result_set);
-        
-        vector<string> result;
-        for(auto it:result_set)    result.push_back(it);
-        return result;        
-    }
-private:
-    void findWords(vector<vector<char>>& board, int x, int y, TrieNode* root, string word, set<string>& result){
-        if(x<0||x>=board.size()||y<0||y>=board[0].size() || board[x][y]==' ') return;
-        
-        if(root->children[board[x][y]-'a'] != NULL){
-            word=word+board[x][y];
-            root=root->children[board[x][y]-'a']; 
-            if(root->is_end) result.insert(word);
-            char c=board[x][y];
-            board[x][y]=' ';
-            findWords(board, x+1, y, root, word, result);
-            findWords(board, x-1, y, root, word, result);
-            findWords(board, x, y+1, root, word, result);
-            findWords(board, x, y-1, root, word, result);
-            board[x][y]=c;        
+        Trie *root = new Trie();
+        trieFunction tF;
+        for(int i = 0; i < words.size(); i++) {
+            tF.addWords(root, words[i]);
         }
+        vector<string> output;
+        set<string> tempOutput;
+        int N = board.size();
+        int M = board[0].size();
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                tF.WS(board, root, i, j, N, M, tempOutput, "");
+            }
+        }
+        for(auto it : tempOutput) {
+            output.push_back(it);
+        }
+        return output;
     }
 };
