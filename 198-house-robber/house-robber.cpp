f21f4ed578fc240aff492a96214d3691d62b5263
Vaibhav Mishra <42261169+vmishra-7@users.noreply.github.com>
2024-03-23 21:30:41 +0530

    Time: 4 ms (32.41%) | Memory: 9.3 MB (10.56%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int rob(vector<int> nums, int i, int *DP) {
+    int rob(vector<int>& nums, int i) {
         if(i >= nums.size()) {
             return 0;
         }
-        if(DP[i] != -1) {
-            return DP[i];
-        }
-        return DP[i] = max(nums[i] + rob(nums, i + 2, DP), rob(nums, i + 1, DP));
+        return max(rob(nums, i + 2) + nums[i], rob(nums, i + 1));
     }
     int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) {
+            return 0;
+        }
+        if(n == 1) {
+            return nums[0];
+        }
         int *DP = new int[nums.size()];
-        for(int i = 0; i < nums.size(); i++) {
-            DP[i] = -1;
+        DP[0] = nums[0];
+        DP[1] = max(nums[0], nums[1]);
+        for(int i = 2; i < nums.size(); i++) {
+            DP[i] = max(DP[i - 1], DP[i - 2] + nums[i]);
         }
-        int out = rob(nums, 0, DP);
-        for(int i = 0; i < nums.size(); i++) {
-            cout << DP[i] << " ";
-        }
-        return out;
+        return DP[nums.size() - 1];
     }
 };
