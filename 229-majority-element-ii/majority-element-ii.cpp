302e449ba0f93f4400f3fe97f5c397069a0c0e88
Vaibhav Mishra <42261169+vmishra-7@users.noreply.github.com>
2024-03-25 16:40:05 +0530

    Time: 8 ms (66.73%) | Memory: 18.6 MB (35.33%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -4,13 +4,16 @@ public:
         sort(nums.begin(), nums.end());
         int thirdMaj = nums.size() / 3;
         vector<int> out;
-        for(int i = 0; i < nums.size() - thirdMaj; i++) {
+        for(int i = 0; i < nums.size() - thirdMaj;) {
             if(out.size() != 0 && out.back() == nums[i]) {
+                i++;
                 continue;
             }
             if(nums[i] == nums[i + thirdMaj]) {
                 out.push_back(nums[i]);
+                i += thirdMaj;
             }
+            i++;
         }
         reverse(out.begin(), out.end());
         return out;
