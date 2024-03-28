de53bfcda912cd6668461581422402e35aa0677b
Vaibhav Mishra <42261169+vmishra-7@users.noreply.github.com>
2024-03-28 20:24:49 +0530

    Time: 7 ms (76.26%) | Memory: 45.5 MB (22.32%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -16,12 +16,11 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int **DP = new int*[nums.size() + 1];
         for(int i = 0; i <= nums.size(); i++) {
-            DP[i] = new int[3001];
+            DP[i] = new int[3001];   //traget + 20 * 1000 (worst case)
             for(int j = 0; j <= 3000; j++) {
                 DP[i][j] = -1;
             }
         }
         return findTargetSumWays(nums, 0, target + 1000, DP);
-        return 1;
     }
 };
