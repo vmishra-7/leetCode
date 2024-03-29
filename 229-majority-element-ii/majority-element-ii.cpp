78806a97ea261d03fe1eeeda00fcfdd7869ee2ca
Vaibhav Mishra <42261169+vmishra-7@users.noreply.github.com>
2024-03-29 23:04:08 +0530

    Time: 7 ms (83.27%) | Memory: 18.2 MB (99.27%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int thirdMaj = nums.size() / 3;
-        vector<int> out;
-        for(int i = 0; i < nums.size() - thirdMaj;) {
-            if(out.size() != 0 && out.back() == nums[i]) {
-                i++;
-                continue;
+        int count1 = 0, candidate1 = INT_MIN, count2 = 0, candidate2 = INT_MIN;
+        for(int i = 0; i < nums.size(); i++) {
+            if(count1 == 0 && nums[i] != candidate2) {
+                candidate1 = nums[i];
+                count1++;
+            } else if(count2 == 0 && nums[i] != candidate1) {
+                candidate2 = nums[i];
+                count2++;
+            } else if(nums[i] == candidate1) {
+                count1++;
+            } else if(nums[i] == candidate2) {
+                count2++;
+            } else {
+                count1--, count2--;
             }
-            if(nums[i] == nums[i + thirdMaj]) {
-                out.push_back(nums[i]);
-                i += thirdMaj;
-            }
-            i++;
         }
-        reverse(out.begin(), out.end());
+        count1 = 0, count2 = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] == candidate1) {
+                count1++;
+            } else if(nums[i] == candidate2) {
+                count2++;
+            }
+        }
+        vector<int> out;
+        if(count1 > nums.size() / 3) {
+            out.push_back(candidate1);
+        }
+        if(count2 > nums.size() / 3) {
+            out.push_back(candidate2);
+        }
         return out;
     }
 };
